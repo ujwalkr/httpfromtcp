d79c4e06a6118ac8441966db32fe1e779487dd31
Ujwal K Raikar <raikarujwal@gmail.com>
2026-01-02 15:09:22 +0530

    HTTP Request-line parsing

---
+++ internal/headers/headers.go
@@ -0,0 +1,12 @@
+package headers
+
+// type Headers map[string]string
+
+
+// func NewHeaders() Headers {
+// 	return Headers{}
+// }
+
+// func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+
+// 
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -3,7 +3,6 @@ package request
 import (
 	"io"
 	"testing"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
