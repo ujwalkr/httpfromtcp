66c6dfc4bd415f36c43f22d92164e7f79ecfed1e
Ujwal K Raikar <raikarujwal@gmail.com>
2026-01-02 18:10:08 +0530

    Header parsing

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,12 +1,99 @@
 package headers
 
-// type Headers map[string]string
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+)
 
+var rn = []byte("\r\n")
 
-// func NewHeaders() Headers {
-// 	return Headers{}
-// }
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+		if !found {
+			return false
+		}
+	}
 
-// func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	return true
+}
 
-// 
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed field line")
+	}
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("malformed field name")
+	}
+
+	return string(name), string(value), nil
+}
+
+type Headers struct {
+	Headers map[string]string
+}
+
+func NewHeaders() *Headers {
+	return &Headers{
+		Headers: map[string]string{},
+	}
+}
+
+func (h *Headers) Get(name string) string {
+	return h.Headers[strings.ToLower(name)]
+}
+func (h *Headers) Set(name, value string) {
+	name = strings.ToLower(name)
+	if v, ok := h.Headers[name]; ok {
+		h.Headers[name] = fmt.Sprintf("%s,%s", v, value)
+	} else {
+		h.Headers[name] = value
+	}
+}
+
+func (h *Headers) Parse(data []byte) (int, bool, error) {
+	log.Printf("Field Line Original : %v\n", string(data))
+
+	read := 0
+	done := false
+
+	for {
+		idx := bytes.Index(data[read:], rn)
+		if idx == -1 {
+			break
+		}
+
+		//EMPTY Header
+		if idx == 0 {
+			done = true
+			read += len(rn)
+			break
+		}
+		name, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("Malformed header name")
+		}
+		read += idx + len(rn)
+		h.Set(name, value)
+	}
+
+	return read, done, nil
+}
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -1,28 +1,49 @@
 package headers
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestHeaderParse(t *testing.T) {
-// 	// Test: Valid single header
-// 	headers := NewHeaders()
-// 	data := []byte("Host: localhost:42069\r\n\r\n")
-// 	n, done, err := headers.Parse(data)
-// 	require.NoError(t, err)
-// 	require.NotNil(t, headers)
-// 	assert.Equal(t, "localhost:42069", headers["Host"])
-// 	assert.Equal(t, 23, n)
-// 	assert.False(t, done)
+func TestHeaderParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nFooFoo: barbar \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, "barbar", headers.Get("FooFoo"))
+	assert.Equal(t, "", headers.Get("MissingKey"))
 
-// 	// Test: Invalid spacing header
-// 	headers = NewHeaders()
-// 	data = []byte("       Host : localhost:42069       \r\n\r\n")
-// 	n, done, err = headers.Parse(data)
-// 	require.Error(t, err)
-// 	assert.Equal(t, 0, n)
-// 	assert.False(t, done)
-// }
+	assert.NotEqual(t, 23, n)
+	assert.Equal(t, 42, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid header name
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Multiple headers
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: localhost:4  \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069,localhost:4", headers.Get("host"))
+	assert.True(t, done)
+}
